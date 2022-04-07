d9fb9394fbca3cbc235850974ba6a440508e7f0e
mjfrncscoo <francisco.markjim@ue.edu.ph>
2022-04-07 09:55:40 +0800

    Update Settings.jsx
    Settings content

--- components/LMS/Settings.jsx
+++ components/LMS/Settings.jsx
@@ -12,6 +12,49 @@ const Settings = () => {
                 </div>
             </div>
             <h4 className="text-subheading">Change Password</h4>
+
+            <br />
+            <PasswordInput
+                wrapperClassName="w-full mt-2 flex rounded-xl"
+                textFieldClassName="m-auto border-lightGray rounded-xl placeholder-lightGray"
+                classNames="lg:w-full md:w-full sm:w-full xs:w-3/4 xxs:w-3/4 mt-2 rounded-xl border border-lightGray placeholder-lightGray"
+                iconTop={"top-2"}
+                value={password}
+                setValue={setPassword}
+                placeholder="Current Password"
+            />
+            <br />
+            <PasswordInput
+                wrapperClassName="w-full mt-2 flex rounded-xl"
+                textFieldClassName="m-auto border-lightGray rounded-xl placeholder-lightGray"
+                classNames="lg:w-full md:w-full sm:w-full xs:w-3/4 xxs:w-3/4 mt-2 rounded-xl border border-lightGray placeholder-lightGray"
+                iconTop={"top-2"}
+                value={password}
+                setValue={setPassword}
+                placeholder="New Password"
+            />
+            <br />
+            <ConfirmPasswordInput
+                wrapperClassName="lg:w-full md:w-full sm:w-full xs:w-11/12 xxs:w-11/12 mt-2 xs:h-8 xxs:h-8 flex rounded-xl"
+                textFieldClassName="xs:h-8 xxs:h-8 m-auto border-lightGray rounded-xl placeholder-lightGray"
+                classNames="lg:w-full md:w-full sm:w-full xs:w-11/12 xxs:w-11/12 mt-2 rounded-xl border border-lightGray placeholder-lightGray"
+                iconTop={"top-1"}
+                value={confirmPassword}
+                setValue={setConfirmPassword}
+                password={password}
+                placeholder="Re-enter your password"
+            />
+            <br />
+            <button
+                disabled={disable ? true : false}
+                onClick={() => {
+                setCreateAccount(true);
+                }}
+                className="bg-blue-500 text-white text-xl font-semibold lg:w-full md:w-full sm:w-full xs:w-11/12 xxs:w-11/12 lg:mt-10 md:mt-10 sm:mt-2 py-3 rounded-full disabled:opacity-50"
+            >
+                Change Password
+            </button>
+            
         </div>
     </div>
   );
